0c10c0def6e4291b97dc2847cf3c5b5ee677df96
Thomas <thomas_ace8@hotmail.com>
2022-12-20 14:53:19 +0100

    Added Project
    Code First Alpha Solution

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestWebbshopCodeFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
