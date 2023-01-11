3f1bc550d1c67bfdbe2b8210cbce567f34355e45
Bertwald <thomas_ace8@hotmail.com>
2023-01-11 13:21:02 +0100

    New class

---
+++ Logic/ItemSelector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestWebbshopCodeFirst.UserInterface;
+
+namespace TestWebbshopCodeFirst.Logic {
+    internal class ItemSelector<T> where T : class {
+        internal static T? GetItemFromList(List<T> items) {
+            if (!items.Any()) {
+                return null;
+            }
+
+            List<string> itemStrings = new List<string>();
+            foreach (T item in items) {
+                itemStrings.Add(item.ToString()!);
+            }
+            int itemIndex = InputModule.SelectFromList(itemStrings);
+            return items[itemIndex];
+        }
+    }
+}
