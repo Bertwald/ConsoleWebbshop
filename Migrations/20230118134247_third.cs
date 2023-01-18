c78b5e2b4ab9892e79551aa1db47b8a9d40a18cb
Bertwald <thomas_ace8@hotmail.com>
2023-01-18 14:58:41 +0100

    third

---
+++ Migrations/20230118134247_third.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestWebbshopCodeFirst.Migrations
+{
+    public partial class third : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShippingAdresses",
+                table: "Customers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingAdresses",
+                table: "Customers",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+    }
+}
